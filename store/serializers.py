518659c7ffd24bc5081987c5279e9c6f1637d229
Aijana2004 <kurmanbekovajzan3@gmail.com>
2024-06-20 10:23:30 +0600

    cwvwpv w vwv wo

---
+++ store/serializers.py
@@ -0,0 +1,23 @@
+from  rest_framework import serializers
+from .models import *
+
+
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model= Category
+        fields='__all__'
+
+
+
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','product_name','description','date','price','category','active']
+
+
+
+class CommentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model= Comment
+        fields='__all__'
